f7a4eefde86c63240193b0b3c17349d595842412
bilkish-akther <bilkishakther921@gmaile.com>
2023-07-02 23:33:46 -0700

    added config file

--- src/config/firebase.init.js
+++ src/config/firebase.init.js
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyARMneeGPF8ccwDqQqWuh8asrobf9bf_ys",
+  authDomain: "sound-safari-f0811.firebaseapp.com",
+  projectId: "sound-safari-f0811",
+  storageBucket: "sound-safari-f0811.appspot.com",
+  messagingSenderId: "380111442365",
+  appId: "1:380111442365:web:f9595d469eccdcb6e242d4"
 };
 
 // Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
